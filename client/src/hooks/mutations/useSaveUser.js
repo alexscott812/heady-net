635d444399bb8a77a956eac3f0813b2c67dcc810
alexscott812 <alexscott812@gmail.com>
2022-09-22 09:48:59 -0400

    save user hook update

--- client/src/hooks/mutations/useSaveUser.js
+++ client/src/hooks/mutations/useSaveUser.js
@@ -1,23 +1,31 @@
 import { useMutation, useQueryClient } from 'react-query';
 import { useAuth } from '../../lib/auth';
+import useToast from '../useToast.js';
 
-// const useSaveUser = ({ onSuccess = null, onError = null }) => {
-const useSaveUser = () => {
+const useSaveUser = (opts = {}) => {
   const { updateAccount } = useAuth();
   const queryClient = useQueryClient();
+  const createToast = useToast();
+
   return useMutation(
     (data) => updateAccount(data), {
       onSuccess: (data) => {
-        console.log(data);
         queryClient.refetchQueries(['users', data._id]);
         queryClient.refetchQueries(['reviews', {
           user_id: data._id, sort: '-created_at'
         }]);
-        //onSuccess && onSuccess();
+        createToast({
+          id: 'edit-user-success',
+          status: 'success',
+          message: 'Profile updated!'
+        });
       },
-      onError: err => {
-        console.log(err);
-      }
+      onError: (err) => createToast({
+        id: 'edit-user-error',
+        status: 'error',
+        message: err
+      }),
+      ...opts
     }
   );
 };
