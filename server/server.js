14a9ed3bd658afae97728edf9043097113211b9b
alexscott812 <alexscott812@gmail.com>
2022-12-02 11:16:39 -0500

    adjusting cors

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 // OPTIONS & MIDDLEWARE
 app.use(cookieParser());
 // app.use(cors({ origin: 'http://192.168.1.42:3000', credentials: true }));
+app.use(cors({ origin: '*' }));
 app.use(logger(':method :url :status - :response-time ms'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
