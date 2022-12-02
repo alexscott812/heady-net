16026095524d3611d68ecda3a4420ecf643280a1
alexscott812 <alexscott812@gmail.com>
2022-12-02 10:59:30 -0500

    prod api url proxy

--- client/src/utils/axios-instance.js
+++ client/src/utils/axios-instance.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5000/api',
   // baseURL: 'http://192.168.1.42:5000/api',
-  baseURL: '/api',
+  // baseURL: '/api',
+  baseURL: 'https://shakedown-api.onrender.com/api',
   headers: {
     'Content-type': 'application/json',
     'Accept': 'application/json'
